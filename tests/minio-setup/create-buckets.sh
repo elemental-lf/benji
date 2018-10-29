a61fe19c0c0d5918f364164b08daf052770ceb27
Lars Fenneberg <lf@elemental.net>
2018-10-29 23:11:22 +0100

    Create buckets used for tests

---
+++ tests/minio-setup/create-buckets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function mc 
+{
+  port=$1; shift
+  command="$@"
+  
+  docker run -it --net=host --entrypoint=/bin/sh minio/mc -c "/usr/bin/mc -q --insecure config host add s3 http://localhost:$port/ minio minio123; /usr/bin/mc -q --insecure $command"
+}
+
+mc 9901 mb s3/benji
+mc 9901 mb s3/benji-2
