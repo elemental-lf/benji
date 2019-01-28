61a0a5ad8b872d91ea2e1add29304117b1c578e8
Lars Fenneberg <lf@elemental.net>
2019-01-28 22:23:36 +0100

    Prometheus: Fix variable name mismatches for new invalid/incomplete metrics

--- scripts/common.sh
+++ scripts/common.sh
@@ -23,9 +23,9 @@ function benji::command {
 }
 
 function benji::versions_status {
-    local INCOMPLETE_VERSIONS=$(benji -m --log-level "$BENJI_LOG_LEVEL" ls 'status == "incomplete" and date < "1 day ago"' | jq '.versions | length')
-    local OLDER_INVALID_VERSIONS=$(benji -m --log-level "$BENJI_LOG_LEVEL" ls 'status == "invalid"' | jq '.versions | length')
+    local OLDER_INCOMPLETE_VERSIONS=$(benji -m --log-level "$BENJI_LOG_LEVEL" ls 'status == "incomplete" and date < "1 day ago"' | jq '.versions | length')
+    local INVALID_VERSIONS=$(benji -m --log-level "$BENJI_LOG_LEVEL" ls 'status == "invalid"' | jq '.versions | length')
 
-    benji_older_incomplete_versions set $OLDER_INVALID_VERSIONS
-    benji_invalid_versions set $INVALID_BACKUPS
+    benji_older_incomplete_versions set $OLDER_INCOMPLETE_VERSIONS
+    benji_invalid_versions set $INVALID_VERSIONS
 }
