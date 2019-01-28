71aa0658ab17600a452f2e3343737b7e4e43f061
Lars Fenneberg <lf@elemental.net>
2019-01-28 23:29:52 +0100

    Add database migration for locking design problem

---
+++ src/benji/sql_migrations/alembic/versions/fe79ce75cefa_fix_locking_design.py
@@ -0,0 +1,27 @@
+"""Fix locking design
+
+Revision ID: fe79ce75cefa
+Revises: b1fa564a0ebf
+Create Date: 2019-01-28 23:19:47.958399
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'fe79ce75cefa'
+down_revision = 'b1fa564a0ebf'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_table('locks')
+    op.create_table('locks', sa.Column('lock_name', sa.String(255), nullable=False, primary_key=True),
+                    sa.Column('host', sa.String(255), nullable=False),
+                    sa.Column('process_id', sa.String(255), nullable=False),
+                    sa.Column('reason', sa.String(255), nullable=False), sa.Column('date', sa.DateTime, nullable=False))
+
+
+def downgrade():
+    pass
