e0b3edca0ad233c69e62c2db7a13ba40f8aa49f5
Lars Fenneberg <lf@elemental.net>
2019-01-31 17:43:49 +0100

    Documentation update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev69+ga8cb9e2.dirty',
+    VERSION: '0.1.1.dev70+g3ac5c1e.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
