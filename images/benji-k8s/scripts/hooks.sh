b8a8c4addfcb543d63478e876781f26fb8c16d55
Lars Fenneberg <lf@elemental.net>
2019-02-21 12:50:50 +0100

    Add stats to Kubernetes event message

--- images/benji-k8s/scripts/hooks.sh
+++ images/benji-k8s/scripts/hooks.sh
@@ -1,5 +1,42 @@
 #!/usr/bin/env bash
 
+function benji::backup::ceph::snapshot::create::pre {
+    local VERSION_NAME="$1"
+    local CEPH_POOL="$2"
+    local CEPH_RBD_IMAGE="$3"
+    local CEPH_RBD_SNAPSHOT="$4"
+
+    echo "Not implemented yet: Freezeing filesystem $CEPH_RBD_IMAGE_MOUNTPOINT on host $K8S_PV_HOST_IP."
+    # FIXME: Implement optional fsfreeze --freeze (switch on/off by label on PVC)
+
+    return 0
+}
+
+function benji::backup::ceph::snapshot::create::post::error {
+    local VERSION_NAME="$1"
+    local CEPH_POOL="$2"
+    local CEPH_RBD_IMAGE="$3"
+    local CEPH_RBD_SNAPSHOT="$4"
+
+    echo "Not implemented yet: Unfreezing filesystem $CEPH_RBD_IMAGE_MOUNTPOINT on host $K8S_PV_HOST_IP."
+    # FIXME: Implement optional fsfreeze --unfreeze (switch on/off by label on PVC)
+
+    return 0
+}
+
+function benji::backup::ceph::snapshot::create::post::success {
+    benji::backup::ceph::snapshot::create::post::error "$@"
+}
+
+function benji::backup::pre {
+    local VERSION_NAME="$1"
+
+    START_TIME=$(date +'%s')
+    benji_backup_start_time -command=backup -auxiliary_data=initial -version_name="$VERSION_NAME" set $(date +'%s.%N')
+
+    return 0
+}
+
 function _k8s_create_pvc_event {
     local TYPE="$1"
     local REASON="$2"
@@ -45,43 +82,6 @@ source:
 EOF
 }
 
-function benji::backup::ceph::snapshot::create::pre {
-    local VERSION_NAME="$1"
-    local CEPH_POOL="$2"
-    local CEPH_RBD_IMAGE="$3"
-    local CEPH_RBD_SNAPSHOT="$4"
-
-    echo "Not implemented yet: Freezeing filesystem $CEPH_RBD_IMAGE_MOUNTPOINT on host $K8S_PV_HOST_IP."
-    # FIXME: Implement optional fsfreeze --freeze (switch on/off by label on PVC)
-
-    return 0
-}
-
-function benji::backup::ceph::snapshot::create::post::error {
-    local VERSION_NAME="$1"
-    local CEPH_POOL="$2"
-    local CEPH_RBD_IMAGE="$3"
-    local CEPH_RBD_SNAPSHOT="$4"
-
-    echo "Not implemented yet: Unfreezing filesystem $CEPH_RBD_IMAGE_MOUNTPOINT on host $K8S_PV_HOST_IP."
-    # FIXME: Implement optional fsfreeze --unfreeze (switch on/off by label on PVC)
-
-    return 0
-}
-
-function benji::backup::ceph::snapshot::create::post::success {
-    benji::backup::ceph::snapshot::create::post::error "$@"
-}
-
-function benji::backup::pre {
-    local VERSION_NAME="$1"
-
-    START_TIME=$(date +'%s')
-    benji_backup_start_time -command=backup -auxiliary_data=initial -version_name="$VERSION_NAME" set $(date +'%s.%N')
-
-    return 0
-}
-
 function benji::backup::post::error {
     local VERSION_NAME="$1"
     local BENJI_BACKUP_STDERR="$2"
@@ -96,6 +96,13 @@ function benji::backup::post::error {
     return 0
 }
 
+function _format_version_stats {
+    local VERSION_UID="$1"
+
+    benji -m --log-level "$BENJI_LOG_LEVEL" stats "uid == '$VERSION_UID'" | \
+        jq -r '.stats[0] | "(took " + (.duration | tostring) + " seconds, " + (.bytes_written | tostring) + " bytes written)"'
+}
+
 function benji::backup::post::success {
     local VERSION_NAME="$1"
     local BENJI_BACKUP_STDERR="$2"
@@ -105,7 +112,8 @@ function benji::backup::post::success {
     benji_backup_completion_time -command=backup -auxiliary_data=initial -version_name="$VERSION_NAME" set $(date +'%s.%N')
     benji_backup_runtime_seconds -command=backup -auxiliary_data=initial -version_name="$VERSION_NAME" set $[$(date +'%s') - $START_TIME]
 
-    _k8s_create_pvc_event Normal SuccessfulBackup "Backup to $VERSION_UID completed successfully"
+    _k8s_create_pvc_event Normal SuccessfulBackup "Backup to $VERSION_UID completed successfully" \
+        "$(_format_version_stats $VERSION_UID)"
 
     return 0
 }
