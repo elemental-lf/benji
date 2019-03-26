17a92271d0fb383a04e3e302801f7985e349b7ca
Lars Fenneberg <lf@elemental.net>
2019-03-26 22:06:26 +0100

    YAPF

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ except ImportError:
 with open('README.rst', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
+
 def get_version_and_cmdclass(package_path):
     import os
     from importlib.util import module_from_spec, spec_from_file_location
-    spec = spec_from_file_location('version',
-                                   os.path.join('src', package_path, '_version.py'))
+    spec = spec_from_file_location('version', os.path.join('src', package_path, '_version.py'))
     module = module_from_spec(spec)
     spec.loader.exec_module(module)
     return module.__version__, module.cmdclass
+
 
 version, cmdclass = get_version_and_cmdclass('benji')
 
