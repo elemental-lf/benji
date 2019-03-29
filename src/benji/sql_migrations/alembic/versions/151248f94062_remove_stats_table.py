0ff625ca71be4f563ddea74198d4a0edcaaef578
Lars Fenneberg <lf@elemental.net>
2019-03-29 23:58:19 +0100

    Use more portable SQL to migrate statistics

--- src/benji/sql_migrations/alembic/versions/151248f94062_remove_stats_table.py
+++ src/benji/sql_migrations/alembic/versions/151248f94062_remove_stats_table.py
@@ -7,7 +7,6 @@ Create Date: 2019-03-28 13:08:27.087076
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = '151248f94062'
@@ -38,17 +37,17 @@ def upgrade():
         batch_op.add_column(sa.Column('bytes_written', sa.BigInteger(), nullable=True))
         batch_op.add_column(sa.Column('duration', sa.BigInteger(), nullable=True))
 
-    op.execute(versions.update().where(versions.c.uid == version_statistics.c.uid).values({
+    op.execute(versions.update().values({
         versions.c.bytes_dedup:
-        version_statistics.c.bytes_dedup,
+        sa.sql.select(columns=[version_statistics.c.bytes_dedup]).where(versions.c.uid == version_statistics.c.uid),
         versions.c.bytes_read:
-        version_statistics.c.bytes_read,
+        sa.sql.select(columns=[version_statistics.c.bytes_read]).where(versions.c.uid == version_statistics.c.uid),
         versions.c.bytes_sparse:
-        version_statistics.c.bytes_sparse,
+        sa.sql.select(columns=[version_statistics.c.bytes_sparse]).where(versions.c.uid == version_statistics.c.uid),
         versions.c.bytes_written:
-        version_statistics.c.bytes_written,
+        sa.sql.select(columns=[version_statistics.c.bytes_written]).where(versions.c.uid == version_statistics.c.uid),
         versions.c.duration:
-        version_statistics.c.duration,
+        sa.sql.select(columns=[version_statistics.c.duration]).where(versions.c.uid == version_statistics.c.uid),
     }))
 
     op.drop_table('version_statistics')
