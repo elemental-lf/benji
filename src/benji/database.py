f6b4d25c27424af83d1936aff90af6ea0ce19fbf
Lars Fenneberg <lf@elemental.net>
2019-07-15 01:34:48 +0200

    Fix DetachedInstanceError when calling benji enforce with --machine-output
    The problem is twofold: Due to the fact that SQLAlchemy expires objects
    on commit reading the labels via the version object fails after the
    version has been deleted in the database. This is fixed by
    expire_on_commit=False. But then reading the labels itself fails because
    they were never present in the session due to the default of lazily
    loading relationships. This is fixed by making sure to always load the
    associated labels when a version is loaded from the database.

--- src/benji/database.py
+++ src/benji/database.py
@@ -12,6 +12,7 @@ import sqlite3
 import time
 import uuid
 from abc import abstractmethod
+from binascii import hexlify, unhexlify
 from contextlib import contextmanager
 from functools import total_ordering
 from typing import Union, List, Tuple, TextIO, Dict, cast, Iterator, Set, Any, Optional, Sequence, Callable
@@ -26,7 +27,6 @@ from alembic import command as alembic_command
 from alembic.config import Config as alembic_config_Config
 from alembic.runtime.environment import EnvironmentContext
 from alembic.script import ScriptDirectory
-from binascii import hexlify, unhexlify
 
 from benji.config import Config
 from benji.exception import InputDataError, InternalError, AlreadyLocked, UsageError
@@ -351,6 +351,8 @@ class Version(Base):
         backref='version',
         order_by='asc(Label.name)',
         passive_deletes=True,
+        # Always load labels together with the version
+        lazy='selectin',
     )
 
     blocks = sqlalchemy.orm.relationship(
@@ -581,7 +583,7 @@ class DatabaseBackend(ReprMixIn):
                 cursor.execute("PRAGMA foreign_keys=ON")
                 cursor.close()
 
-        Session = sqlalchemy.orm.sessionmaker(bind=self._engine)
+        Session = sqlalchemy.orm.sessionmaker(bind=self._engine, expire_on_commit=False)
         self._session = Session()
         self._locking = DatabaseBackendLocking(self._session)
         self._last_blocks_commit = time.monotonic()
