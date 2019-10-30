6becc036ae0702d4b56009ff3c7a00dfe9a04cdd
Lars Fenneberg <lf@elemental.net>
2019-10-30 15:43:44 +0100

    Populate storages table to avoid contraint violations

--- src/benji/sql_migrations/alembic/versions/dd844d630d49_add_table_storages.py
+++ src/benji/sql_migrations/alembic/versions/dd844d630d49_add_table_storages.py
@@ -8,6 +8,8 @@ Create Date: 2019-10-01 00:06:35.657495
 import sqlalchemy as sa
 from alembic import op
 
+from benji.config import Config
+
 # revision identifiers, used by Alembic.
 revision = 'dd844d630d49'
 down_revision = '2bb97229fe36'
@@ -20,10 +22,21 @@ def upgrade():
                     sa.Column('name', sa.String(length=255), nullable=False),
                     sa.PrimaryKeyConstraint('id', name=op.f('pk_storages')),
                     sa.UniqueConstraint('name', name=op.f('uq_storages_name')))
+
+    benji_config = op.get_context().config.attributes.get('benji_config', None)
+    if benji_config is not None:
+        storages = sa.Table('storages', sa.MetaData(), autoload_with=op.get_bind())
+        storages_list = benji_config.get('storages', types=list)
+        for index, storage in enumerate(storages_list):
+            name = Config.get_from_dict(storage, 'name', types=str, index=index)
+            storage_id = Config.get_from_dict(storage, 'storageId', None, types=int, index=index)
+            op.execute(storages.insert().values(name=name, id=storage_id))
+
     with op.batch_alter_table('versions', schema=None) as batch_op:
         batch_op.create_foreign_key(batch_op.f('fk_versions_storage_id_storages'), 'storages', ['storage_id'], ['id'])
     with op.batch_alter_table('deleted_blocks', schema=None) as batch_op:
-        batch_op.create_foreign_key(batch_op.f('fk_deleted_blocks_storage_id_storages'), 'storages', ['storage_id'], ['id'])
+        batch_op.create_foreign_key(batch_op.f('fk_deleted_blocks_storage_id_storages'), 'storages', ['storage_id'],
+                                    ['id'])
 
 
 def downgrade():
