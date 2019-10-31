0539a4a859ef2637ba21ca801df3523f8d527ff9
Lars Fenneberg <lf@elemental.net>
2019-10-31 01:24:13 +0100

    Fix typo in method name

--- src/benji/sql_migrations/alembic/versions/da25cc147a07_volume_version_uid.py
+++ src/benji/sql_migrations/alembic/versions/da25cc147a07_volume_version_uid.py
@@ -134,7 +134,7 @@ def upgrade():
     with op.batch_alter_table('labels', schema=None) as batch_op:
         batch_op.drop_index(batch_op.f('ix_labels_value'))
     with op.batch_alter_table('labels_new', schema=None) as batch_op:
-        batch_op.create_ikndex(batch_op.f('ix_labels_name'), ['name'], unique=False)
+        batch_op.create_index(batch_op.f('ix_labels_name'), ['name'], unique=False)
         batch_op.create_index(batch_op.f('ix_labels_value'), ['value'], unique=False)
 
     labels = sa.Table('labels', metadata, autoload_with=conn)
