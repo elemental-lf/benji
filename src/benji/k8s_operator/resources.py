f5739c9a5805ce3d1f2fa3898d70651da17b10ad
Lars Fenneberg <lf@elemental.net>
2020-03-25 23:36:46 +0100

    Remove CromJob related functions

--- src/benji/k8s_operator/resources.py
+++ src/benji/k8s_operator/resources.py
@@ -63,60 +63,6 @@ def create_job(command: List[str], *, parent_body: Dict[str, Any], logger) -> ku
     return batch_v1_api.create_namespaced_job(namespace=service_account_namespace(), body=job_manifest)
 
 
-def create_cron_job(command: List[str],
-                    schedule: str,
-                    *,
-                    parent_body: Dict[str, Any],
-                    logger,
-                    name_override: str = None) -> kubernetes.client.models.v1_job.V1Job:
-    if benji.k8s_operator.operator_config is None:
-        raise RuntimeError('Operator configuration has not been loaded.')
-
-    cron_job_manifest = copy.deepcopy(benji.k8s_operator.operator_config['spec']['cronJobTemplate'])
-    setup_manifest(manifest=cron_job_manifest,
-                   namespace=service_account_namespace(),
-                   parent_body=parent_body,
-                   name_override=name_override)
-
-    cron_job_manifest['spec']['schedule'] = schedule
-    cron_job_manifest['spec']['jobTemplate']['spec']['template']['spec']['containers'][0]['command'] = command
-    cron_job_manifest['spec']['jobTemplate']['spec']['template']['spec']['containers'][0]['args'] = []
-
-    # Actually create the cron job via the Kubernetes API.
-    logger.debug(f'Creating CronJob: {cron_job_manifest}')
-    batch_v1beta1_api = kubernetes.client.BatchV1beta1Api()
-    return batch_v1beta1_api.create_namespaced_cron_job(namespace=service_account_namespace(), body=cron_job_manifest)
-
-
-def delete_cron_job(name: str, namespace: str, *, logger) -> None:
-    batch_v1beta1_api = kubernetes.client.BatchV1beta1Api()
-
-    try:
-        logger.debug(f'Deleting CronJob {namespace}/{name}.')
-        batch_v1beta1_api.delete_namespaced_cron_job(name=name, namespace=namespace)
-    except ApiException as exception:
-        if exception.status == 404:
-            # CronJob went way in the meantime
-            pass
-
-
-def get_cron_jobs(
-        parent_body: Dict[str, Any]) -> List[kubernetes.client.api_client.models.v1beta1_cron_job.V1beta1CronJob]:
-    kind = parent_body['kind']
-    namespace = parent_body['metadata'].get('namespace', None)
-    name = parent_body['metadata']['name']
-    if namespace is not None:
-        label_selector = f'{LABEL_PARENT_KIND}={kind},{LABEL_PARENT_NAMESPACE}={namespace},{LABEL_PARENT_NAME}={name}'
-    else:
-        label_selector = f'{LABEL_PARENT_KIND}={kind},{LABEL_PARENT_NAME}={name}'
-
-    batch_v1beta1_api = kubernetes.client.BatchV1beta1Api()
-    cron_job_list = batch_v1beta1_api.list_namespaced_cron_job(namespace=service_account_namespace(),
-                                                               label_selector=label_selector)
-
-    return cron_job_list.items
-
-
 def create_object_ref(resource_dict: Dict[str, Any], include_gvk: bool = True) -> Dict[str, Any]:
     reference = {
         'name': resource_dict['metadata']['name'],
@@ -126,7 +72,7 @@ def create_object_ref(resource_dict: Dict[str, Any], include_gvk: bool = True) -
 
     if include_gvk:
         reference.update({
-            # This is a difference between the dicts provided by Kopf and the official Kubernetes clients.
+            # There is a difference between the dicts provided by Kopf and the official Kubernetes client.
             'apiVersion':
                 resource_dict['apiVersion'] if 'apiVersion' in resource_dict else resource_dict['api_version'],
             'kind':
