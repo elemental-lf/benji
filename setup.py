798067b3c967619abf445c5e6b254d93869d7634
Lars Fenneberg <lf@elemental.net>
2020-09-21 13:06:24 +0200

    Update to setup.py to include Python 3.8

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)
 Operating System :: POSIX
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 Topic :: System :: Archiving :: Backup
 """[:-1].split('\n'),
     keywords='backup',
