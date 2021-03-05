e767fc89d4911a6f5ad2fc600b5b8dcc7e2d5159
Lars Fenneberg <lf@elemental.net>
2021-03-05 14:21:56 +0100

    Also export __path__ as a public symbol
    I don' really know if this is correct but I thought that __all__ should
    list all declared symbols.

--- src/benji/__init__.py
+++ src/benji/__init__.py
@@ -1,5 +1,5 @@
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-__all__ = ['__version__']
+__all__ = ['__version__', '__path__']
 from ._version import __version__
 del _version  # remove to avoid confusion with __version__
